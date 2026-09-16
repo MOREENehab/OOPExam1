74bdc2799b3ac43ecabfe8e8330a3b9b84f21901
MOREENehab <moreenehab@icloud.com>
2026-09-17 01:49:10 +0300

    Question

---
+++ OOPExam1/Question.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExam1
+{
+    public abstract class Question
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+
+        public Answer[] Answers { get; set; }
+        public Answer RightAnswer { get; set; }
+
+        protected Question(
+            string header,
+            string body,
+            int mark,
+            Answer[] answers,
+            Answer rightAnswer)
+        {
+            Header = header;
+            Body = body;
+            Mark = mark;
+            Answers = answers;
+            RightAnswer = rightAnswer;
+        }
+
+        public override string ToString()
+        {
+            return $"{Header}: {Body} ({Mark} Marks)";
+        }
+    }
+}
