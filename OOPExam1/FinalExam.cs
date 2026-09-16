a3cbef6439a35b30531368f5083c892304c11c58
MOREENehab <moreenehab@icloud.com>
2026-09-17 01:55:48 +0300

    Exams

---
+++ OOPExam1/FinalExam.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExam1
+{
+    public class FinalExam : Exam
+    {
+        public FinalExam(
+            int time,
+            int numberOfQuestions,
+            Question[] questions)
+            : base(time, numberOfQuestions, questions)
+        {
+        }
+
+        public override void ShowExam()
+        {
+            Console.WriteLine("===== Final Exam =====");
+
+            foreach (Question question in Questions)
+            {
+                Console.WriteLine(question);
+
+                foreach (Answer answer in question.Answers)
+                {
+                    Console.WriteLine(answer);
+                }
+
+                Console.WriteLine(
+                    $"Right Answer: {question.RightAnswer}");
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Grade: ...");
+        }
+    }
+}
---
+++ OOPExam1/PracticalExam.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExam1
+{
+    public class PracticalExam : Exam
+    {
+        public PracticalExam(
+            int time,
+            int numberOfQuestions,
+            Question[] questions)
+            : base(time, numberOfQuestions, questions)
+        {
+        }
+
+        public override void ShowExam()
+        {
+            Console.WriteLine("===== Practical Exam =====");
+
+            foreach (Question question in Questions)
+            {
+                Console.WriteLine(question);
+
+                Console.WriteLine(
+                    $"Right Answer: {question.RightAnswer}");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
