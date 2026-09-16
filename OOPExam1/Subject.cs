ef159fe6e2357ebf0205856add0dbc1a5a5cc7b5
MOREENehab <moreenehab@icloud.com>
2026-09-17 01:56:54 +0300

    Subject

---
+++ OOPExam1/Subject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExam1
+{
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public Exam Exam { get; set; }
+
+        public Subject(int id, string name)
+        {
+            SubjectId = id;
+            SubjectName = name;
+        }
+
+        public void CreateExam(Exam exam)
+        {
+            Exam = exam;
+        }
+
+        public override string ToString()
+        {
+            return $"{SubjectId} - {SubjectName}";
+        }
+    }
+}
