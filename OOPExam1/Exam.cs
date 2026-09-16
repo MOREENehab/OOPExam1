230415c759e4e16dcac127650988e3e2dc815955
MOREENehab <moreenehab@icloud.com>
2026-09-17 01:53:28 +0300

    exam

---
+++ OOPExam1/Exam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExam1
+{
+    public abstract class Exam
+    {
+        public int Time { get; set; }
+        public int NumberOfQuestions { get; set; }
+
+        public Question[] Questions { get; set; }
+
+        protected Exam(int time, int numberOfQuestions, Question[] questions)
+        {
+            Time = time;
+            NumberOfQuestions = numberOfQuestions;
+            Questions = questions;
+        }
+
+        public abstract void ShowExam();
+    }
+}
